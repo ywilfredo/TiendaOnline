0a0c3c69ae9cf9dc12dd645141b669b3edbb8fe8
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-03-18 09:38:50 -0400

    Agregar archivos de proyecto.

---
+++ TiendaOnline/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TiendaOnline.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
